cac885b205245c223c2938b9da896913206368db
yoursenpai02 <159107953+yoursenpai02@users.noreply.github.com>
2024-04-01 01:02:36 +0530

    Update __main__.py

--- NekoRobot/__main__.py
+++ NekoRobot/__main__.py
@@ -164,11 +164,11 @@ HELP_STRINGS = """
 """
 
 GROUP_START_IMG = (
-    "https://te.legra.ph/file/5f272dc498e4cf91faf92.jpg",
-    "https://te.legra.ph/file/9d8190aba9ac3421af488.jpg",
-    "https://te.legra.ph/file/5b1ce336f0c0216224040.jpg",
-    "https://te.legra.ph/file/f1f67047d973f3aea4004.jpg",
-    "https://te.legra.ph/file/c2526c05d65fd2fbfa58b.jpg",
+    "https://telegra.ph/file/ab7cddfa540604b72253d.jpg",
+    "https://telegra.ph/file/ab7cddfa540604b72253d.jpg",
+    "https://telegra.ph/file/ab7cddfa540604b72253d.jpg",
+    "https://telegra.ph/file/ab7cddfa540604b72253d.jpg",
+    "https://telegra.ph/file/ab7cddfa540604b72253d.jpg",
 )
 
 DONATE_STRING = """❂ I'm Free for Everyone ❂"""
